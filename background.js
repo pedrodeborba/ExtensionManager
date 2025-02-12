60c827c1bc56e47e5c596222af2feb26b5e45187
Pedro de Borba <pedrodeborba1709@gmail.com>
2025-02-12 08:46:53 -0300

    release: :bookmark: version 1.1

--- background.js
+++ background.js
@@ -1,14 +1,15 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url.includes('google.com.br')) {
-      const extensionId = 'dpfofggmkhdbfcciajfdphofclabnogo'; // ID da extensão a ser desativada
-      chrome.management.setEnabled(extensionId, false, () => {
-        console.log('Extensão desativada.');
-      });
-    } else if (changeInfo.status === 'complete' && tab.url.includes('http://catm.usaflex.com.br:3000/auth')) {
-        const extensionId = 'dpfofggmkhdbfcciajfdphofclabnogo'; // ID da extensão a ser ativada
-        chrome.management.setEnabled(extensionId, true, () => {
-          console.log('Extensão ativada.');
-        });
-    }
-  });
-  
+  if (changeInfo.status === 'complete' && tab.url) {
+      const extensionId = 'dpfofggmkhdbfcciajfdphofclabnogo';
+
+      if (tab.url.includes('google.com.br')) {
+          chrome.management.setEnabled(extensionId, false, () => {
+              console.log('Extensão desativada.');
+          });
+      } else if (tab.url.includes('http://catm.usaflex.com.br:3000/auth')) {
+          chrome.management.setEnabled(extensionId, true, () => {
+              console.log('Extensão ativada.');
+          });
+      }
+  }
+});
